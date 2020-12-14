656b3480fb58128e443bd256280da10b1a918343
HafiszahSaidatul <hafiszahsaidatull@gmail.com>
2020-12-14 21:37:59 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+const selectElement = function(element) {
+    return document.querySelector(element);
+};
+
+let menuToggler = selectElement('.menu-toggle');
+let body = selectElement('body');
+
+menuToggler.addEventListener('click', function() {
+    body.classList.toggle('open');
+    hero.classList.add('open');
+});
+navlist.addEventListener('click', function(e) {
+    let clickedelement = e.target;
+    if (clickedelement.classList[0] === "nav-link") {
+        bodyelement.classList.remove('is-open');
+    }
+
+});
+window.addEventListener('click', function(e) {
+    let clickedelement = e.target;
+
+    if (clickedelement.matches('.open')) {
+        bodyedlement.classList.remove('open');
+        heroedlement.classList.remove('open')
+    }
+
+});
